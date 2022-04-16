0a6e91425a40fbcd4fb1e1e612899d0bcc53e18b
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-04-16 11:47:54 +0200

    Nested loop added to index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Creamos el tablero
+// Creamos el tablero con un nested for
+
+const board = {
+  rows: 10,
+  columns: 10,
+};
+
+for (let x = 0; x < board.rows; y++) {
+  for (let y = 0; y < board.columns; x++) {
+    // Do something;
+  }
+}
